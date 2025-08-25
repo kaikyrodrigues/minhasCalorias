9344531867c91a6095c62e8643e80f87412d7262
kaikyrodrigues <kaikyrodriguesrosa@hotmail.com>
2025-08-25 19:16:25 -0300

    Atualiza layout e scripts + adiciona novas páginas e ícones

--- script.js/alimentos.js
+++ script.js/alimentos.js
@@ -1,1 +1,473 @@
-export const alimentos = [...]
+export const proteinas=[
+    {
+        nome: 'Coxa de frango',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 26,
+        gord: 11.06,
+        carb: 0,
+        kcal: 214,
+        imagem: './icons/proteinas/coxa-de-frango.png'
+    },
+    {
+        nome: 'Peito de frango',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 29.55,
+        gord: 7.72,
+        carb: 0,
+        kcal: 195,
+        imagem:'./icons/proteinas/peito-de-frango(1).png'
+    },
+    {
+        nome: 'Sobre coxa',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 25.94,
+        gord: 10.88,
+        carb: 0,
+        kcal: 209,
+        imagem:'./icons/proteinas/sobre-coxa.png'
+    },
+    {
+        nome: 'File de peito',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 23,
+        gord: 1,
+        carb: 0,
+        kcal: 101,
+        imagem:'./icons/proteinas/peito-de-frango.png'
+    },
+    {
+        nome: 'Ovo',
+        quantidade: 1,
+        unidade: 'und',
+        prot: 6.29,
+        gord: 4.97,
+        carb: 0.38,
+        kcal: 74,
+        imagem:'./icons/proteinas/ovo-cozido.png'
+    },
+    {
+        nome: 'Salmão',
+        quantidade:100,
+        unidade:'g',
+        prot: 21.62,
+        gord: 5.93,
+        carb: 0,
+        kcal: 146,
+        imagem:'./icons/proteinas/salmao.png'
+    },
+    {
+        nome: 'Tilápia',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 20.08,
+        gord: 1.7,
+        carb: 0,
+        kcal: 96,
+        imagem:'./icons/proteinas/tilapia.png'
+    },
+    {
+        nome: 'Atum',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 23.38,
+        gord: 0.95,
+        carb: 0,
+        kcal: 108,
+        imagem:'./icons/proteinas/atum.png'
+    },
+    {
+        nome: 'Patinho',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 24.55,
+        gord: 2.43,
+        carb: 0,
+        kcal: 127,
+        imagem:'./icons/proteinas/patinho.png'
+    },
+    {
+        nome: 'Alcatra',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 20.68,
+        gord: 7.81,
+        carb: 0,
+        kcal: 108,
+        imagem:'./icons/proteinas/alcatra.png'
+    },
+    {
+        nome: 'Contra-filé',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 21.95,
+        gord: 4.68,
+        carb: 0,
+        kcal: 135,
+        imagem:'./icons/proteinas/contra-file.png'
+    },
+    {
+        nome: 'Coxão mole',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 21.24,
+        gord: 8.58,
+        carb: 0,
+        kcal: 169,
+        imagem:'./icons/proteinas/coxao.png'
+    },
+    {
+        nome: 'Coxão duro',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 21.00,
+        gord: 4.3,
+        carb: 0,
+        kcal: 121,
+        imagem:'./icons/proteinas/coxao.png'
+    },
+    {
+        nome: 'Acém',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 20.00,
+        gord: 10.00,
+        carb: 0,
+        kcal: 200,
+        imagem:'./icons/proteinas/acem.png'
+    },
+    {
+        nome: 'Musculo',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 22.12,
+        gord: 6.52,
+        carb: 0,
+        kcal: 152,
+        imagem:'./icons/proteinas/musculo.png'
+    },
+    {
+        nome: 'Lombo',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 27.00,
+        gord: 6.00,
+        carb: 0,
+        kcal: 190,
+        imagem:'./icons/proteinas/porco.png'
+    },
+    {
+        nome: 'Pernil',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 25.00,
+        gord: 9.00,
+        carb: 0,
+        kcal: 210,
+        imagem:'./icons/proteinas/porco.png'
+    },
+    {
+        nome: 'Costela',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 21.38,
+        gord: 16.00,
+        carb: 0,
+        kcal: 260,
+        imagem:'./icons/proteinas/porco.png'
+    }
+
+        
+];
+
+export const proteinasVegetais =[
+    {
+        nome: 'Feijão Carioca',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 4.8,
+        gord: 0.5,
+        carb: 13.6,
+        kcal: 76,
+        imagem:'./icons/proteinas-vegetais/feijao.png'
+    },
+    {
+        nome: 'Feijão preto',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 8.86,
+        gord: 0.54,
+        carb: 23.71,
+        kcal: 132,
+        imagem:'./icons/proteinas-vegetais/feijao-preto.png'
+    },
+    {
+        nome: 'Lentilha',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 8.39,
+        gord: 6.76,
+        carb: 18.73,
+        kcal: 165,
+        imagem:'./icons/proteinas-vegetais/lentilhas.png'
+    },
+    {
+        nome: 'Grão de bico',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 8.86,
+        gord: 2.59,
+        carb: 27.42,
+        kcal: 164,
+        imagem:'./icons/proteinas-vegetais/grao-de-bico.png'
+    },
+    {
+        nome: 'Ervilha seca',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 24.0,
+        gord: 1.0,
+        carb: 64,
+        kcal: 340,
+        imagem:'./icons/proteinas-vegetais/ervilha.png'
+    },
+    {
+        nome: 'Soja grão',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 35.22,
+        gord: 25.4,
+        carb: 33.55,
+        kcal: 471,
+        imagem:'./icons/proteinas-vegetais/soja.png'
+    }
+];
+
+export const carboidratos =[
+    {
+        nome: 'Arroz',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 2.7,
+        gord: 0.3,
+        carb: 28,
+        kcal: 130,
+        imagem:'./icons/carboidratos/arroz.png'
+    },
+    {
+        nome: 'Pão francês',
+        quantidade: 1,
+        unidade: 'und',
+        prot: 4.5,
+        gord: 1.5,
+        carb: 26,
+        kcal: 135,
+        imagem:'./icons/carboidratos/pao.png'
+    },
+    {
+        nome: 'Batata inglesa',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 2.0,
+        gord: 0.1,
+        carb: 20,
+        kcal: 86,
+        imagem:'./icons/carboidratos/potato.png'
+    },
+    {
+        nome: 'Batata doce',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 1.6,
+        gord: 0.1,
+        carb: 21,
+        kcal: 86,
+        imagem:'./icons/carboidratos/batata-doce.png'
+    },
+    {
+        nome: 'Macarrão',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 5,
+        gord: 1.1,
+        carb: 25,
+        kcal: 131,
+        imagem:'./icons/carboidratos/macarrao.png'
+    },
+    {
+        nome: 'Aveia flocos',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 16,
+        gord: 7,
+        carb: 66,
+        kcal: 389,
+        imagem:'./icons/carboidratos/aveia.png'
+    },
+    {
+        nome: 'Banana prata',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 1.1,
+        gord: 0.3,
+        carb: 23,
+        kcal: 89,
+        imagem:'./icons/carboidratos/bananas.png'
+    },
+    {
+        nome: 'Feijão CA cozido',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 8.5,
+        gord: 0.5,
+        carb: 14,
+        kcal: 127,
+        imagem:'./icons/carboidratos/feijao.png'
+    },
+    {
+        nome: 'Milho cozido',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 3.4,
+        gord: 1.5,
+        carb: 19,
+        kcal: 96,
+        imagem:'./icons/carboidratos/milho.png'
+    },
+    {
+        nome: 'Mandioca cozida',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 1.1,
+        gord: 0.3,
+        carb: 30,
+        kcal: 125,
+        imagem:'./icons/carboidratos/mandioca.png'
+    }
+];
+
+export const frutas =[
+    {
+        nome: 'Banana',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 1.3,
+        gord: 0.3,
+        carb: 22.8,
+        kcal: 96,
+        imagem:'./icons/frutas/bananas.png'
+    },
+    {
+        nome: 'Maçã',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.3,
+        gord: 0.2,
+        carb: 13.8,
+        kcal: 52,
+        imagem:'./icons/frutas/maca.png'
+    },
+    {
+        nome: 'Laranja',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.9,
+        gord: 0.1,
+        carb: 11.8,
+        kcal: 47,
+        imagem:'./icons/frutas/laranja.png'
+    },
+    {
+        nome: 'Mamão',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.5,
+        gord: 0.1,
+        carb: 10.8,
+        kcal: 43,
+        imagem:'./icons/frutas/mamao.png'
+    },
+    {
+        nome: 'Uva',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.6,
+        gord: 0.2,
+        carb: 17.0,
+        kcal: 69,
+        imagem:'./icons/frutas/uva.png'
+    },
+    {
+        nome: 'Manga',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.6,
+        gord: 0.4,
+        carb: 14.9,
+        kcal: 60,
+        imagem:'./icons/frutas/manga.png'
+    },
+    {
+        nome: 'Melancia',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.6,
+        gord: 0.2,
+        carb: 7.6,
+        kcal: 30,
+        imagem:'./icons/frutas/melancia.png'
+    },
+    {
+        nome: 'Abacaxi',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.9,
+        gord: 0.1,
+        carb: 13.1,
+        kcal: 50,
+        imagem:'./icons/frutas/abacaxi.png'
+    },
+    {
+        nome: 'Morango',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.8,
+        gord: 0.3,
+        carb: 7.7,
+        kcal: 32,
+        imagem:'./icons/frutas/morango.png'
+    },
+    {
+        nome: 'Pêra',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 0.4,
+        gord: 0.1,
+        carb: 15.0,
+        kcal: 57,
+        imagem:'./icons/frutas/pera.png'
+    },
+    {
+        nome: 'Goiaba',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 2.6,
+        gord: 0.9,
+        carb: 14.3,
+        kcal: 68,
+        imagem:'./icons/frutas/goiaba.png'
+    },
+    {
+        nome: 'Abacate',
+        quantidade: 100,
+        unidade: 'g',
+        prot: 1.0,
+        gord: 15.0,
+        carb: 8.5,
+        kcal: 160,
+        imagem:'./icons/frutas/abacate.png'
+    }
+]
--- script.js/script.js
+++ script.js/script.js
@@ -1,102 +1,37 @@
 const btnProteinas = document.querySelector("#btnProteinas")
+const btnProtVege = document.querySelector("#protVeg")
+const btnCarbo = document.querySelector("#btnCarboidratos")
+const btnFrutas = document.querySelector("#btnFrutas")
 const mesgBoasVindas = document.querySelector("#mensagem-boas-vindas")
 const listaDeAlimentos = document.querySelector("#lista-alimentos")
 const imgAlientos = document.querySelector("#img-alimentos")
 
+import {proteinas, proteinasVegetais, carboidratos, frutas} from './alimentos.js'
 
-const proteinas=[
-    {
-        nome: 'Coxa de frango',
-        quantidade: 100,
-        unidade: 'g',
-        prot: 26,
-        gord: 11.06,
-        carb: 0,
-        kcal: 214,
-        imagem: './icons/proteinas/coxa-de-frango.png'
-    },
-    {
-        nome: 'Peito de frango',
-        quantidade: 100,
-        unidade: 'g',
-        prot: 29.55,
-        gord: 7.72,
-        carb: 0,
-        kcal: 195,
-        imagem:'./icons/proteinas/peito-de-frango(1).png'
-    },
-    {
-        nome: 'Sobre coxa',
-        quantidade: 100,
-        unidade: 'g',
-        prot: 25.94,
-        gord: 10.88,
-        carb: 0,
-        kcal: 209,
-        imagem:'./icons/proteinas/sobre-coxa.png'
-    },
-    {
-        nome: 'File de peito',
-        quantidade: 100,
-        unidade: 'g',
-        prot: 23,
-        gord: 1,
-        carb: 0,
-        kcal: 101,
-        imagem:'./icons/proteinas/peito-de-frango.png'
-    },
-    {
-        nome: 'Ovo',
-        quantidade: 100,
-        unidade: 'g',
-        prot: 6.29,
-        gord: 4.97,
-        carb: 0.38,
-        kcal: 74,
-        imagem:'./icons/proteinas/ovo-cozido.png'
-    },
-    {
-        nome: 'Salmão',
-        quantidade:100,
-        unidade:'g',
-        prot: 21.62,
-        gord: 5.93,
-        carb: 0,
-        kcal: 146,
-        imagem:'./icons/proteinas/salmao.png'
-    },
-    {
-        nome: 'Tilápia',
-        quantidade: 100,
-        unidade: 'g',
-        prot: 20.08,
-        gord: 1.7,
-        carb: 0,
-        kcal: 96,
-        imagem:'./icons/proteinas/tilapia.png'
-    },
-    {
-        nome: 'Atum',
-        quantidade: 100,
-        unidade: 'g',
-        prot: 23.38,
-        gord: 0.95,
-        carb: 0,
-        kcal: 108,
-        imagem:'./icons/proteinas/atum.png'
-    }
-
-        
-];
+btnCarbo.addEventListener("click", ()=>{
+    addItens(carboidratos);
+})
 
 btnProteinas.addEventListener("click", ()=>{
-    mesgBoasVindas.style.display = 'none';
-    imgAlientos.style.display = 'none';
+    addItens(proteinas);
+});
+
+btnProtVege.addEventListener("click", ()=>{
+    addItens(proteinasVegetais);
+});
+
+btnFrutas.addEventListener("click", ()=>{
+    addItens(frutas);
+});
+
+function addItens(elemento){
+    mesgBoasVindas.style.display = "none";
+    imgAlientos.style.display = "none";
 
     listaDeAlimentos.innerHTML= "";
     listaDeAlimentos.style.display = "flex";
 
-    proteinas.forEach(item => {
+    elemento.forEach(item =>{
         const div = document.createElement('div');
         div.classList.add('alimento', 'fade-in');
         div.innerHTML = `
@@ -105,5 +40,5 @@ btnProteinas.addEventListener("click", ()=>{
       <span>Carbo: ${item.carb}g</span><span>Kcal: ${item.kcal}</span>
         `
         listaDeAlimentos.appendChild(div);
-    });
-});
+    })
+}
