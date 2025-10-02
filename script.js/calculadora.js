d4dae15750a35fe881f96f9b07eca7f3f0e4eba0
kaikyrodrigues <kaikyrodriguesrosa@hotmail.com>
2025-10-02 17:13:37 -0300

    adicionei menu responsivo e pesquisa

---
+++ script.js/calculadora.js
@@ -0,0 +1,32 @@
+const resultado  = document.querySelector("#result-bioptipo");
+const btnCalcular = document.querySelector("#btnCalcular")
+const btnLimpar = document.querySelector("#btnLimpar")
+
+btnCalcular.addEventListener("click", ()=> {
+    const metabolismo = document.querySelector("#metabolismo").value;
+    const peso = document.querySelector("#peso").value;
+    const estrutura = document.querySelector("#estrutura").value;
+    console.log(metabolismo, peso, estrutura)
+if(metabolismo === "rapido" && peso === "nao" && estrutura === "magro"){
+    resultado.innerHTML = "Seu biotipo é: ECTOMORFO (magro, com dificuldade de ganhar peso)";
+    btnLimpar.style.display = 'block';
+}
+else if(metabolismo ==="normal" && estrutura === "medio"){
+    resultado.innerHTML = "Seu biotipo é: MESOMORFO (fácil ganho de massa muscular)";
+    btnLimpar.style.display = 'block';
+}
+else if(metabolismo === "lento" && peso === "sim" && estrutura ==="largo"){
+    resultado.innerHTML = "Seu biotipo é: ENDOMORFO (tendência a acumular gordura)";
+    btnLimpar.style.display = 'block';
+}
+else{
+    resultado.innerHTML = "Você tem características mistas (biotipo misto)";
+    btnLimpar.style.display = 'block';
+}
+
+});
+
+btnLimpar.addEventListener("click", ()=>{
+    resultado.innerHTML = "";
+    btnLimpar.style.display = 'none';
+})
--- script.js/script.js
+++ script.js/script.js
@@ -5,8 +5,11 @@ const btnFrutas = document.querySelector("#btnFrutas")
 const mesgBoasVindas = document.querySelector("#mensagem-boas-vindas")
 const listaDeAlimentos = document.querySelector("#lista-alimentos")
 const imgAlientos = document.querySelector("#img-alimentos")
+const pesquisar = document.querySelector("#pesquisar")
 
 import {proteinas, proteinasVegetais, carboidratos, frutas} from './alimentos.js'
+
+const alimentos = [...proteinas,...proteinasVegetais,...carboidratos,...frutas]
 
 btnCarbo.addEventListener("click", ()=>{
     addItens(carboidratos);
@@ -41,4 +44,43 @@ function addItens(elemento){
         `
         listaDeAlimentos.appendChild(div);
     })
-}
+};
+
+function mostrarPesquisa(lista){
+    mesgBoasVindas.style.display = "none";
+    imgAlientos.style.display = "none";
+    listaDeAlimentos.innerHTML = "";
+    listaDeAlimentos.style.display = "flex";
+    
+    
+    lista.forEach(item => {
+        const div = document.createElement("div");
+        div.classList.add('alimento', 'fade-in');
+        div.innerHTML = `
+        <img src="${item.imagem}","style="width:32px; height:32px;">
+      <span><strong>${item.nome}</strong></span><span>Proteinas: ${item.prot}g</span><span>Gorduras: ${item.gord}g</span>
+      <span>Carbo: ${item.carb}g</span><span>Kcal: ${item.kcal}</span>
+        `
+        listaDeAlimentos.appendChild(div)
+    })
+};
+
+pesquisar.addEventListener("input", (e)=>{
+    const texto = e.target.value.toLowerCase().trim()
+
+    if(texto === ""){
+        listaDeAlimentos.innerHTML = "";
+        mesgBoasVindas.style.display = "block";
+        imgAlientos.style.display = "block";
+        return
+    }
+
+    const filtrados = alimentos.filter(el =>
+        el.nome.toLocaleLowerCase().includes(texto)
+    );
+    mostrarPesquisa(filtrados)
+})
+
+
+
+
